fd36253c7cab274aaccee62706ee213b692ea244
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2025-04-20 00:49:01 +0700

    add cname and favicon

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function updateContent(format) {
   const newTitle = `Image to ${label} Converter - Free & Fast Online Tool`;
   pageTitle.textContent = newTitle;
   mainHeading.textContent = `Image to ${label} Converter`;
-  mainSubtext.textContent = `Free, lightweight, and browser-based 🌱. Convert your Images to ${label} locally.`;
+  mainSubtext.textContent = `Free, lightweight, and browser-based. Convert your Images to ${label} locally or offline 🌱.`;
 }
 
 outputFormatSelect.addEventListener("change", function () {
